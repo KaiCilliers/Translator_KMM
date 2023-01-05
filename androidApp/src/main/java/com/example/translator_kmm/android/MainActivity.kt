e611a218d47aa6da8974a9c935de34cf0e7caed0
KaiCilliers <kaijohan428@gmail.com>
2023-01-05 11:46:36 +0000

    setup android theme colors and fonts

--- androidApp/src/main/java/com/example/translator_kmm/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/translator_kmm/android/MainActivity.kt
@@ -3,38 +3,33 @@ package com.example.translator_kmm.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.translator_kmm.Greeting
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MyApplicationTheme {
+            TranslatorTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    GreetingView(Greeting().greet())
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun GreetingView(text: String) {
-    Text(text = text)
-}
-
-@Preview
-@Composable
-fun DefaultPreview() {
-    MyApplicationTheme {
-        GreetingView("Hello, Android!")
     }
 }
---
+++ shared/src/commonMain/kotlin/com/example/translator_kmm/core/presentation/Colors.kt
@@ -0,0 +1,9 @@
+package com.example.translator_kmm.core.presentation
+
+object Colors {
+    const val LightBlue: Long = 0xFFA8A5BB
+    const val LightBlueGrey: Long = 0xFFF6F4F4
+    const val AccentViolet: Long = 0xFF5643C9
+    const val TextBlack: Long = 0xFF111111
+    const val DarkGrey: Long = 0xFF282C31
+}
