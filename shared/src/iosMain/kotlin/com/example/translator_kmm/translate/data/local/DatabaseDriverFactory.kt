8d2e111f13bd26f40ec2e01591fc8c1e79408129
KaiCilliers <kaijohan428@gmail.com>
2023-01-05 09:12:33 +0000

    local sqlite database setup

---
+++ shared/src/iosMain/kotlin/com/example/translator_kmm/translate/data/local/DatabaseDriverFactory.kt
@@ -0,0 +1,14 @@
+package com.example.translator_kmm.translate.data.local
+
+import com.example.translator_kmm.database.TranslateDatabase
+import com.squareup.sqldelight.db.SqlDriver
+import com.squareup.sqldelight.drivers.native.NativeSqliteDriver
+
+actual class DatabaseDriverFactory {
+    actual fun create(): SqlDriver {
+        return NativeSqliteDriver(
+            TranslateDatabase.Schema,
+            "translate.db"
+        )
+    }
+}
