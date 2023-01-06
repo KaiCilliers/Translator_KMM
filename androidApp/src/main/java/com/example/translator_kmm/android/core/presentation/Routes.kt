c0d1034457a22cb07947094b5c64fb0345476f90
KaiCilliers <kaijohan428@gmail.com>
2023-01-06 13:32:08 +0000

    add app routes

---
+++ androidApp/src/main/java/com/example/translator_kmm/android/core/presentation/Routes.kt
@@ -0,0 +1,6 @@
+package com.example.translator_kmm.android.core.presentation
+
+object Routes {
+    const val TRANSLATE_SCREEN = "translate_screen"
+    const val VOICE_TO_TEXT_SCREEN = "voice_to_text_screen"
+}
