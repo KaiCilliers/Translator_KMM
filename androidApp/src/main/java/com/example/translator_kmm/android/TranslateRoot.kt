89224671ebcf55211cc3c7a4722e89ec960f2a16
KaiCilliers <kaijohan428@gmail.com>
2023-01-07 13:20:09 +0000

    add support for list of previous translations made and navigation to the text to speech screen

--- androidApp/src/main/java/com/example/translator_kmm/android/TranslateRoot.kt
+++ androidApp/src/main/java/com/example/translator_kmm/android/TranslateRoot.kt
@@ -1,15 +1,19 @@
 package com.example.translator_kmm.android
 
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.translator_kmm.android.core.presentation.Routes
 import com.example.translator_kmm.android.translate.presentation.AndroidTranslateViewModel
 import com.example.translator_kmm.android.translate.presentation.TranslateScreen
+import com.example.translator_kmm.translate.presentation.TranslateEvent
 
 @Composable
 fun TranslateRoot() {
@@ -23,8 +27,28 @@ fun TranslateRoot() {
             val state by viewModel.state.collectAsState()
             TranslateScreen(
                 state = state,
-                onEvent = viewModel::onEvent
+                onEvent = { event ->
+                    when(event) {
+                        is TranslateEvent.RecordAudio -> {
+                            navController.navigate(
+                                Routes.VOICE_TO_TEXT_SCREEN + "/${state.fromLanguage.language.langCode}"
+                            )
+                        }
+                        else -> viewModel.onEvent(event)
+                    }
+                }
             )
+        }
+        composable(
+            route = Routes.VOICE_TO_TEXT_SCREEN + "/{languageCode}",
+            arguments = listOf(
+                navArgument("languageCode") {
+                    type = NavType.StringType
+                    defaultValue = "en"
+                }
+            )
+        ) {
+            Text(text = "Voice-to-Text")
         }
     }
 }
